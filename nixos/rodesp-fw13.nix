80d388f947cdcd5dab8568d01f0e6ce5ac1d7f8d
RodEsp <1084688+RodEsp@users.noreply.github.com>
2024-11-07 23:11:18 -0500

    19 current  2024-11-07 23:11:16  24.05.6171.3c2f1c4ca372  6.6.59                          *

--- nixos/rodesp-fw13.nix
+++ nixos/rodesp-fw13.nix
@@ -143,6 +143,7 @@ in {
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     alejandra
+    eza
     fastfetch
     fprintd
     framework-tool
