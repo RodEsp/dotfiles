11b390cc2db9e8dcdb20a4659689ec22bfcffe0f
RodEsp <1084688+RodEsp@users.noreply.github.com>
2024-11-08 01:10:22 -0500

    21 current  2024-11-08 01:10:17  24.05.6171.3c2f1c4ca372  6.6.59                          *

--- nixos/rodesp-fw13.nix
+++ nixos/rodesp-fw13.nix
@@ -87,6 +87,8 @@ in {
     xwayland.enable = true;
   };
 
+  programs.waybar.enable = true;
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -99,7 +101,7 @@ in {
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -158,6 +160,11 @@ in {
     wget
     wl-clipboard
     unstable.warp-terminal
+
+    # Hyprland
+    dunst # notification daemon
+    rofi-wayland # app launcher
+    swww # wallpaper daemon
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
