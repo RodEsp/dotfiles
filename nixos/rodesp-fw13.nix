3905373a787b410f99b82e303118ccd07dc41d4a
RodEsp <1084688+RodEsp@users.noreply.github.com>
2024-11-10 16:54:40 -0500

    49 current  2024-11-10 16:54:35  24.05.6258.dba414932936  6.6.59                          *

--- nixos/rodesp-fw13.nix
+++ nixos/rodesp-fw13.nix
@@ -96,7 +96,7 @@ in {
   programs.waybar.enable = true;
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
+    # xdg-desktop-portal-gtk
     xdg-desktop-portal-hyprland
   ];
 
