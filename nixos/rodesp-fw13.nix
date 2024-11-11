2f84ed580700d7b05137e6e13640ea79f645ade5
RodEsp <1084688+RodEsp@users.noreply.github.com>
2024-11-11 18:20:12 -0500

    64 current  2024-11-11 18:20:03  24.05.6463.83fb6c028368  6.6.59                          *

--- nixos/rodesp-fw13.nix
+++ nixos/rodesp-fw13.nix
@@ -200,7 +200,7 @@ in {
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.ssh.startAgent = true;
+  # programs.ssh.startAgent = true; # Can't enable this at the same time as gnupg.enableSSHSupport
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
