bfc36d5954e80240f38b79823e55097914be58a2
RodEsp <1084688+RodEsp@users.noreply.github.com>
2024-11-12 17:00:19 -0500

    79 current  2024-11-12 16:31:50  24.05.6529.9256f7c71a19  6.6.60                          *

--- nixos/rodesp-fw13.nix
+++ nixos/rodesp-fw13.nix
@@ -212,6 +212,8 @@ in {
     unstable.hyprpolkitagent
   ];
 
+  services.espanso.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
