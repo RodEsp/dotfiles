1272123a932d8553689bff87b0fabf510f3004ce
RodEsp <1084688+RodEsp@users.noreply.github.com>
2024-11-12 13:04:45 -0500

    feat(main-user): add main user module

---
+++ nixos/main-user.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options = {
+    main-user.enable = lib.mkEnableOption "enable user module";
+
+    main-user.userName = lib.mkOption {
+      default = "rodesp";
+      description = ''
+        username
+      '';
+    };
+  };
+
+  config = lib.mkIf config.main-user.enable {
+    users.users.${config.main-user.userName} = {
+      isNormalUser = true;
+      initialPassword = "12345";
+      description = "Rodrigo Espinosa de los Monteros";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+    };
+  };
+}
