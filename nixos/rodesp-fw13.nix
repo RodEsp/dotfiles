cabb3ac8cdaaac3835fec5ac00aac7beaa63fb0e
RodEsp <1084688+RodEsp@users.noreply.github.com>
2024-11-16 17:35:39 -0500

    89 current  2024-11-16 17:35:35  24.05.6565.689fed12a013  6.6.60                          *

--- nixos/rodesp-fw13.nix
+++ nixos/rodesp-fw13.nix
@@ -119,7 +119,7 @@ in {
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
