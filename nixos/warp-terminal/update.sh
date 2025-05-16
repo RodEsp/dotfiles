029142a98b8b5c28a6553080f95ac79b1805bd2c
RodEsp <1084688+RodEsp@users.noreply.github.com>
2025-05-16 13:24:48 -0400

    feat(warp): update & update.sh improvements

--- nixos/warp-terminal/update.sh
+++ nixos/warp-terminal/update.sh
@@ -1,12 +1,17 @@
 cd ~/git/nixpkgs/pkgs/by-name/wa/warp-terminal
 git checkout upstream-master
+git reset --hard
 git pull --all --rebase
 echo ""
 echo "$(git --no-pager log -1 --format=%cd --date=local)"
 echo ""
+git checkout master
 git reset --hard upstream/master
 git push
 ./update.sh
 cp versions.json ~/dotfiles/nixos/warp-terminal/versions.json
 cd ~/dotfiles/nixos/warp-terminal/
 echo "$(git diff versions.json)"
+
+echo "Warp Terminal version has been updated."
+echo "Please run 'sudo nixos-rebuild switch'"
